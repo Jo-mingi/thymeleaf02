9596e82242a717c914ad97fc3f1854751b72843c
Jo-mingi <mingijo1004@naver.com>
2022-01-21 09:35:46 +0900

    22/01/20 thymeleaf day02

---
+++ src/main/java/co/mr/thymeleaf02/dto/ItemDto.java
@@ -0,0 +1,17 @@
+package co.mr.thymeleaf02.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ItemDto {
+
+    private Long id;
+    private  String name;
+    private Integer price;
+    private String detail;
+    private String sellStatCd;
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+}
